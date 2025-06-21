ebd2e8670a9c5fd54864d7134e5cba23de36133a
newts0fa <nbtvor@yandex.ru>
2025-06-21 14:20:47 +0300

    Update lab3(3).py

--- lab3(3).py
+++ lab3(3).py
@@ -1,20 +1,10 @@
-import unittest
-from quick_sort import quick_sort  
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
 
-class TestQuickSort(unittest.TestCase):
-    def test_sort(self):
-        test_cases = [
-            ([10, 7, 8, 9, 1, 5], [1, 5, 7, 8, 9, 10]),
-            ([5, 3, 8, 6, 2, 7, 1, 4], [1, 2, 3, 4, 5, 6, 7, 8]),
-            ([], []),
-            ([3], [3])
-        ]
-        
-        for arr, expected in test_cases:
-            with self.subTest(arr=arr):
-                arr_copy = arr.copy()
-                quick_sort(arr_copy, 0, len(arr_copy)-1)
-                self.assertEqual(arr_copy, expected)
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
 
-if __name__ == '__main__':
-    unittest.main()
+    return quick_sort(left) + middle + quick_sort(right)
