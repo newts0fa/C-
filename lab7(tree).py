35099b2bd32e6a4c0b6ca22db13f4b89145bda2b
newts0fa <nbtvor@yandex.ru>
2025-06-22 22:34:27 +0300

    Create lab7(tree).py

---
+++ lab7(tree).py
@@ -0,0 +1,14 @@
+def func(n):
+    if n < 0 or n > 20:
+        return 0
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    for i in range(1, n + 1):
+        for j in range(i):
+            dp[i] += dp[j] * dp[i - j - 1]
+    return dp[n]
+
+if __name__ == "__main__":
+    n = int(input())
+    print(func(n))
+
